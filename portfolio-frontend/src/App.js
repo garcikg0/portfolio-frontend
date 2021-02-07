7e321c709df8910f6ccfb88f5f5a5c86489ffd35
garcikg0 <kg.gar91@gmail.com>
2021-02-06 22:46:59 -0500

    properly installed react router sass

--- portfolio-frontend/src/App.js
+++ portfolio-frontend/src/App.js
@@ -1,39 +1,43 @@
 import React from 'react';
-import { Switch, Router, withRouter, Redirect } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <>
-    <div class="first-flex">
-      <div class="developer-name">
-        <h1>Name</h1>
-        <p>Quick Headline with languages  </p>
-        {/* <div class="icons">
-          <i class="fa fa-2x fa-github"></i>
-          <i class="fa fa-2x fa-medium"></i>
-          <i class="fa fa-2x fa-envelope-square"></i>
-          <i class="fa fa-2x fa-twitter"></i>
-          <i class="fa fa-2x fa-instagram"></i>
-        </div> */}
-      </div>
-      <a href="mailto:myemail">Let's Chat</a>
-    </div>
-    <div class="first-flex">
-      <h2>My Work</h2>
-      <div class="card">
-        <h6>Title</h6>
-        <p>Subtitle</p>
-      </div>
-      <div class="card">
-        <h6>Title</h6>
-        <p>Subtitle</p>  
-      </div>
-      <div class="card">
-        <h6>Title</h6>
-        <p>Subtitle</p>
-      </div>
-    </div>
+    <Switch>
+      <Route path='/' >
+        <div class="first-flex">
+          <div class="developer-name">
+            <h1>Name</h1>
+            <p>Quick Headline with languages  </p>
+            {/* <div class="icons">
+              <i class="fa fa-2x fa-github"></i>
+              <i class="fa fa-2x fa-medium"></i>
+              <i class="fa fa-2x fa-envelope-square"></i>
+              <i class="fa fa-2x fa-twitter"></i>
+              <i class="fa fa-2x fa-instagram"></i>
+            </div> */}
+          </div>
+        <a href="mailto:myemail">Let's Chat</a>
+        </div>
+        <div class="first-flex">
+          <h2>My Work</h2>
+          <div class="card">
+            <h6>Title</h6>
+            <p>Subtitle</p>
+          </div>
+          <div class="card">
+            <h6>Title</h6>
+            <p>Subtitle</p>  
+          </div>
+          <div class="card">
+            <h6>Title</h6>
+            <p>Subtitle</p>
+          </div>
+        </div>
+      </Route>
+    </Switch>
     </>
   );
 }
