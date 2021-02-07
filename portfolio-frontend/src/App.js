7b8e25e834c2b67ad6edd60b3063345c7b471c20
garcikg0 <kg.gar91@gmail.com>
2021-02-06 22:14:08 -0500

    landing page in App.js

--- portfolio-frontend/src/App.js
+++ portfolio-frontend/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Switch, Router, withRouter, Redirect } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div class="first-flex">
+      <div class="developer-name">
+        <h1>Name</h1>
+        <p>Quick Headline with languages  </p>
+        {/* <div class="icons">
+          <i class="fa fa-2x fa-github"></i>
+          <i class="fa fa-2x fa-medium"></i>
+          <i class="fa fa-2x fa-envelope-square"></i>
+          <i class="fa fa-2x fa-twitter"></i>
+          <i class="fa fa-2x fa-instagram"></i>
+        </div> */}
+      </div>
+      <a href="mailto:myemail">Let's Chat</a>
     </div>
+    <div class="first-flex">
+      <h2>My Work</h2>
+      <div class="card">
+        <h6>Title</h6>
+        <p>Subtitle</p>
+      </div>
+      <div class="card">
+        <h6>Title</h6>
+        <p>Subtitle</p>  
+      </div>
+      <div class="card">
+        <h6>Title</h6>
+        <p>Subtitle</p>
+      </div>
+    </div>
+    </>
   );
 }
 
-export default App;
+export default withRouter(App);
